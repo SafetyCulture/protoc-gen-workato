ffcc9e1e917f8ca180808bb61f9dbacc5460fc24
Brandon Cook <me@brandonjc.com>
2021-12-08 15:55:16 +1100

    [INTG-2057] Allow generated definition to be accessed when customising object
    This change allows the generated definition to be accessed and extended when customising an object definition

--- templates/object_definitions.tmpl.rb
+++ templates/object_definitions.tmpl.rb
@@ -2,14 +2,16 @@
 {{- range . }}
   "{{ .Key }}": {
     fields: lambda do |connection, config_fields, object_definitions|
-      {{- if .Exec }}
-        {{- .Exec | nindent 6 }}
-      {{- else }}
-      [
+      definition = [
         {{- range .Fields }}
           {{- include "field" . | nindent 8 }},
         {{- end}}
       ]
+
+      {{- if .Exec }}
+        {{- .Exec | nindent 6 }}
+      {{- else }}
+      definition
       {{- end }}
     end
   },
