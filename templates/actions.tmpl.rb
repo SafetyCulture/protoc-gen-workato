60460f91f2a201e79ab098694685c95136daa884
Mick Stanciu <mick.stanciu@safetyculture.io>
2022-01-25 13:19:43 +1100

    [INTG-2103] code cleaning (#37)
    * INTG-2103 use ' when there is no interpolation
    
    * INTG-2103 snake case
    
    * INTG-2103 use && instead of and
    
    * INTG-2103 use && instead of and
    
    * INTG-2103 add paranthesis
    
    * INTG-2103 update test

--- templates/actions.tmpl.rb
+++ templates/actions.tmpl.rb
@@ -9,18 +9,18 @@
     help: lambda do |input, picklist_label|
       case input['action_name']
       {{- range $name, $message := $action.HelpMessages }}
-      when "{{ $name }}"
+      when '{{ $name }}'
         {
-          body: "{{ $message.Body }}",
-          learn_more_url: "{{ $message.LearnMoreURL }}",
-          learn_more_text: "{{ $message.LearnMoreText }}"
+          body: '{{ $message.Body }}',
+          learn_more_url: '{{ $message.LearnMoreURL }}',
+          learn_more_text: '{{ $message.LearnMoreText }}'
         }
       {{- end }}
       else
         {
-          body: "{{ $action.DefaultHelpMessage.Body }}",
-          learn_more_url: "{{ $action.DefaultHelpMessage.LearnMoreURL }}",
-          learn_more_text: "{{ $action.DefaultHelpMessage.LearnMoreText }}"
+          body: '{{ $action.DefaultHelpMessage.Body }}',
+          learn_more_url: '{{ $action.DefaultHelpMessage.LearnMoreURL }}',
+          learn_more_text: '{{ $action.DefaultHelpMessage.LearnMoreText }}'
         }
       end
     end,
@@ -32,17 +32,17 @@
     input_fields: lambda do |object_definitions, connection, config_fields|
       case config_fields['action_name']
       {{- range $name, $object_name := $action.InputFields }}
-      when "{{ $name }}"
-        object_definitions["{{ $object_name }}"]
+      when '{{ $name }}'
+        object_definitions['{{ $object_name }}']
       {{- end }}
       end
     end,
     execute: lambda do |connection, input, eis, eos, continue|
       case input['action_name']
       {{- range $name, $value := $action.ExecCode }}
-      when "{{ $name }}"
-        excludeKeys = {{ format_string_slice $value.ExcludeFromQuery }}
-        body = input.select {|k,v| k != "action_name" and not excludeKeys.include? k }
+      when '{{ $name }}'
+        exclude_keys = {{ format_string_slice $value.ExcludeFromQuery }}
+        body = input.select { |k, v| k != 'action_name' and not exclude_keys.include? k }
         {{ $value.Aggregate | indent 8 | trim }}
       {{- end }}
       end
@@ -50,8 +50,8 @@
     output_fields: lambda do |object_definitions, connection, config_fields|
       case config_fields['action_name']
       {{- range $name, $object_name := $action.OutputFields }}
-      when "{{ $name }}"
-        object_definitions["{{ $object_name }}"]
+      when '{{ $name }}'
+        object_definitions['{{ $object_name }}']
       {{- end }}
       end
     end,
